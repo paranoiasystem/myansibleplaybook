57bea6266665e35ab5207a91399a5228eaa665ec
Marco Ferraioli <m.ferraioli25@studenti.unisa.it>
2017-09-21 14:30:25 +0200

    Create stopdeletecontainer.sh

---
+++ stopdeletecontainer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+result=$( docker ps | grep $1 | grep -o "^[0-9a-z]*" )
+
+if [[ -n "$result" ]]; then
+  echo "Container run, now stop it"
+  docker stop $result
+  docker rm $result
+fi
